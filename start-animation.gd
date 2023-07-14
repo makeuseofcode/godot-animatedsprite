8622a1565c400eb2c42ee4f9f1771e097219179d
Imran Alam <75203259+Imranalam28@users.noreply.github.com>
2023-07-14 13:09:17 +0530

    Create start-animation.gd

---
+++ start-animation.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+const SPEED = 200
+
+func _physics_process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= SPEED
+	if Input.is_action_pressed("move_right"):
+		velocity.x += SPEED
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= SPEED
+	if Input.is_action_pressed("move_down"):
+		velocity.y += SPEED
+		
+	# Play the animation when the "play_animation" action is triggered
+	if Input.is_action_just_pressed("play_animation"):
+		$AnimatedSprite.play()
+
+	# Stop the animation and reset to the first frame when the "stop_animation" action is triggered
+	if Input.is_action_just_pressed("stop_animation"):
+		$AnimatedSprite.stop()
+		$AnimatedSprite.frame = 0
+		
+	velocity = move_and_slide(velocity)
+	
+func _ready():
+	pass 
